e2e6063b2313e59fc8620e83e2da78f6601b54d0
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-02-01 23:39:57 +0530

    adding personal info

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
        Account bobsAccount = new Account();
+       bobsAccount.setNumber("2134533");
+       bobsAccount.setbalance(1000.0);
+       bobsAccount.setCustomerName("Bob Green");
+       bobsAccount.setCustomerEmail("myaccount@bob.com");
+       bobsAccount.setCustomerPhone("(090-12535)");
        bobsAccount.withdrawFunds(100.0);
        bobsAccount.depositFunds(250.0);
        bobsAccount.withdrawFunds(50.0);
