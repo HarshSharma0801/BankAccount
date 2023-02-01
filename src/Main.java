ea3cc54b556a073383e785eb3fe90c31a15734c9
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-02-02 00:03:21 +0530

    adding Statements

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,13 @@ public class Main {
        bobsAccount.setCustomerName("Bob Green");
        bobsAccount.setCustomerEmail("myaccount@bob.com");
        bobsAccount.setCustomerPhone("(090-12535)");
+        System.out.println("Name - "+bobsAccount.getCustomerName());
+        System.out.println("Balance - "+bobsAccount.getbalance());
+        bobsAccount.depositFunds(1000.0);
+
        bobsAccount.withdrawFunds(100.0);
-       bobsAccount.depositFunds(250.0);
-       bobsAccount.withdrawFunds(50.0);
-       bobsAccount.withdrawFunds(200.0);
-        bobsAccount.depositFunds(100.0);
-        bobsAccount.withdrawFunds(45.55);
-        bobsAccount.withdrawFunds(54.55);
-        bobsAccount.withdrawFunds(54.45);
+
+
 
 
 
